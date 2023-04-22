09bddb90e7a185cfcdb46bca8fdafbce344d0d18
Zac Poorman <poortexanlegos@gmail.com>
2023-04-22 10:48:30 -0600

    initialized with Next

---
+++ src/menus/hamburger.tsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import { slide as Menu } from "react-burger-menu";
+const HamburgerMenu = () => (
+  <div className="relative p-2">
+    <Menu
+      customBurgerIcon={<HamburgerIcon />}
+      width={"auto"}
+      className="left-0 top-12"
+    >
+      <Links />
+    </Menu>
+  </div>
+);
+
+const HamburgerIcon = () => (
+  <div className="p-1/2">
+    <svg
+      className="w-8 h-8 text-gray-500"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path d="M4 6h16M4 12h16M4 18h16"></path>
+    </svg>
+  </div>
+);
+
+export const Links = () => (
+  <>
+    <Link id="home" className="menu-item" href="/">
+      Home
+    </Link>
+    <Link id="scripture" className="menu-item" href="/scripture">
+      Scripture Passages
+    </Link>
+    <Link id="download" className="menu-item" href="/download">
+      Download Templates
+    </Link>
+    <Link id="update" className="menu-item" href="/update">
+      Project Registration
+    </Link>
+    <Link id="search" className="menu-item" href="/search">
+      Search
+    </Link>
+    <Link id="folder" className="menu-item" href="/folder">
+      Folder
+    </Link>
+    <Link id="demo" className="menu-item" href="/demo">
+      Add Demo
+    </Link>
+    <Link id="sync" className="menu-item" href="/sync">
+      Sync
+    </Link>
+    <Link id="about" className="menu-item" href="/about">
+      About
+    </Link>
+  </>
+);
+
+export default HamburgerMenu;
