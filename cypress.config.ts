76b9d0aefdecbfd0e0ce764dd072a86142b69b22
Zac Poorman <poortexanlegos@gmail.com>
2023-04-22 21:48:08 -0600

    added cypress config stuff

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('@cypress/code-coverage/task')(on, config)
+      // include any other plugin code...
+
+      // It's IMPORTANT to return the config object
+      // with any changed environment variables
+      return config
     },
   },
-});
+})
---
+++ instrumented/pages/index.tsx
@@ -0,0 +1,71 @@
+import { Card, CardContent, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useEffect } from "react";
+import axios from "axios";
+import Image from "next/image";
+
+interface JSONData {
+  name: string;
+  description: string;
+  image: string;
+  key: string;
+}
+
+const Scripture = () => {
+  const [toggle, setToggle] = React.useState(true);
+  const [arrayData, setArrayData] = React.useState<JSONData[]>([]);
+
+  useEffect(() => {
+    setToggle(false);
+    async function getData() {
+      if (toggle) {
+        await axios.get("/api/example").then((response) => {
+          for (let int in response.data) {
+            setArrayData((arrayData) => [...arrayData, response.data[int]]);
+          }
+
+          console.log(arrayData);
+        });
+        return;
+      }
+    }
+    getData();
+  }, [arrayData, toggle]);
+
+  return (
+    <div className="main-contianer" style={{ paddingTop: "5vh" }}>
+      <Card sx={{ ml: "15vw", mr: "15vw" }}>
+        <CardContent>
+          <Typography variant="h3" style={{ textAlign: "center" }}>
+            Story Template
+          </Typography>
+        </CardContent>
+      </Card>
+
+      {arrayData.map((element, key) => (
+        <Card variant="outlined" sx={{ m: "5vw" }} key={element.key}>
+          <CardContent>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignContent: "flex-start",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                src={element.image}
+                alt={element.description}
+                width={100}
+                height={100}
+              />
+              <Typography sx={{ ml: "10vw" }}>{element.name}</Typography>
+            </Box>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default Scripture;
