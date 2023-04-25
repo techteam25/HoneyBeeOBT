f0171fbce885fcca2b348101520fc6539459d203
Zac Poorman <poortexanlegos@gmail.com>
2023-04-24 18:49:12 -0600

    added download template with elements linked

---
+++ src/pages/api/download.ts
@@ -0,0 +1,13 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+    res.status(200).json({ record1: {name: "Jesus Walking", description: "Example description", image: "/jesusWalking.jpg", link: "https://techteam.org/tech-team-advantage-solutions/software-solutions/", key: "story1"}, record2: {name: "Discovering Empty Tomb", description: "Here is example Description 2", image: "/emptyTomb.jpg", link: "https://techteam.org/tech-team-advantage-solutions/software-solutions/", key: "story2"}, record3: {name: "Empty Tomb", description: "Here is more info", image: "/tomb2.jpg", link: "https://techteam.org/tech-team-advantage-solutions/software-solutions/", key: "story3"}})
+}
---
+++ src/pages/download.tsx
@@ -0,0 +1,78 @@
+import { Card, CardContent, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useEffect } from "react";
+import axios from "axios";
+import Image from "next/image";
+import { act } from "@testing-library/react";
+import Link from "next/link";
+
+interface JSONData {
+  name: string;
+  description: string;
+  image: string;
+  link: string;
+  key: string;
+}
+
+const Download = () => {
+  const [toggle, setToggle] = React.useState(true);
+  const [arrayData, setArrayData] = React.useState<JSONData[]>([]);
+
+  useEffect(() => {
+    setToggle(false);
+    async function getData() {
+      if (toggle) {
+        await axios.get("/api/download").then((response) => {
+          for (let int in response.data) {
+            act(() => {
+              setArrayData((arrayData) => [...arrayData, response.data[int]]);
+            });
+          }
+
+          console.log(arrayData);
+        });
+        return;
+      }
+    }
+    getData();
+  }, [arrayData, toggle]);
+
+  return (
+    <div className="main-contianer" style={{ paddingTop: "5vh" }}>
+      <Card sx={{ ml: "15vw", mr: "15vw" }}>
+        <CardContent>
+          <Typography variant="h3" style={{ textAlign: "center" }}>
+            Download Templates
+          </Typography>
+        </CardContent>
+      </Card>
+
+      {arrayData.map((element) => (
+        <Card variant="outlined" sx={{ m: "5vw" }} key={element.key}>
+          <Link href={element.link}>
+            <CardContent>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignContent: "flex-start",
+                  alignItems: "center",
+                }}
+              >
+                <Image
+                  src={element.image}
+                  alt={element.description}
+                  width={100}
+                  height={100}
+                />
+                <Typography sx={{ ml: "10vw" }}>{element.name}</Typography>
+              </Box>
+            </CardContent>
+          </Link>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default Download;
