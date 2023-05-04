2df186280e16cfe3e2e386ed0b168093fbb24223
Zac Poorman <poortexanlegos@gmail.com>
2023-05-03 22:10:37 -0600

    added experimental middleware for ratelimiting

--- src/pages/api/assets.ts
+++ src/pages/api/assets.ts
@@ -2,12 +2,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import path from 'path';
 import { promises as fs } from 'fs';
+import { ipRateLimit } from '@/components/menus/middleware/ipRateLimit';
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+  req: NextApiRequest & Request,
+  res: NextApiResponse,
 ) {
-    const jsonDirectory = path.join(process.cwd(), 'public/assets/demo/project');
-    const fileContents = await fs.readFile(jsonDirectory + '/story.json', "utf-8");
-    res.status(200).json(JSON.parse(fileContents))
+  const jsonDirectory = path.join(process.cwd(), 'public/assets/demo/project');
+  const fileContents = await fs.readFile(jsonDirectory + '/story.json', "utf-8");
+  /* Commented out for rate limiter
+  const res = await ipRateLimit(req)
+  if (res.status !== 200) return res
+    res.headers.set('content-type', 'application/json')
+    return new Response(JSON.parse((fileContents)))
+  */  
+  res.status(200).json(JSON.parse(fileContents))
 }
