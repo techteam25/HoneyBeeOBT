b3ab04b16a9e1575402c621aefc906d8b32c38d5
Zac Poorman <poortexanlegos@gmail.com>
2023-06-26 22:59:09 -0600

    added rudimentary reate limiting to assets endpoint

--- src/pages/api/assets.ts
+++ src/pages/api/assets.ts
@@ -2,19 +2,47 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import path from 'path';
 import { promises as fs } from 'fs';
-import { ipRateLimit } from '@/components/middleware/ipRateLimit';
+import rateLimit from 'express-rate-limit'
+import slowDown from 'express-slow-down'
 
-export default async function handler(
-  req: NextApiRequest & Request,
-  res: NextApiResponse,
-) {
+const getIP = (request: any) =>
+  request.ip ||
+  request.headers['x-forwarded-for'] ||
+  request.headers['x-real-ip'] ||
+  request.connection.remoteAddress
+
+const limit = 10
+const windowMs = 60 * 1_000
+const delayAfter = Math.round(limit / 2)
+const applyMiddleware = (middleware: any) => (request: any, response: any) =>
+  new Promise((resolve, reject) => {
+    middleware(request, response, (result: unknown) =>
+      result instanceof Error ? reject(result) : resolve(result)
+    )
+  })
+const delayMs = 500
+
+export const getRateLimitMiddlewares = ({
+  limit = 10,
+  windowMs = 60 * 1000,
+  delayAfter = Math.round(10 / 2),
+  delayMs = 500,
+} = {}) => [
+  slowDown({ keyGenerator: getIP, windowMs, delayAfter, delayMs }),
+  rateLimit({ keyGenerator: getIP, windowMs, max: limit }),
+]
+
+const middlewares = getRateLimitMiddlewares({ limit: 50 }).map(applyMiddleware)
+
+export default async function handler(req: any, res: { status: (arg0: number) => { (): any; new(): any; send: { (arg0: string): any; new(): any; }; json: { (arg0: any): void; new(): any; }; }; }) {
+  try {
+    await Promise.all(
+      middlewares.map(middleware => middleware(req, res))
+    )
+  } catch {
+    return res.status(429).send('Too Many Requests')
+  }
   const jsonDirectory = path.join(process.cwd(), 'public/assets/demo/project');
   const fileContents = await fs.readFile(jsonDirectory + '/story.json', "utf-8");
-  /* Commented out for rate limiter
-  const res = await ipRateLimit(req)
-  if (res.status !== 200) return res
-    res.headers.set('content-type', 'application/json')
-    return new Response(JSON.parse((fileContents)))
-  */  
   res.status(200).json(JSON.parse(fileContents))
 }
