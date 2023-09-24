7b34ec3d4c7e8e34de051fa4d07e17a81d44aa3c
Zac Poorman <poortexanlegos@gmail.com>
2023-09-24 01:43:24 -0600

    removed some config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
     },
   });
   return (
-    <UserProvider loginUrl="/api/auth/login" profileUrl="/api/auth/me">
+    <UserProvider>
       <ThemeProvider theme={darkTheme}>
         <title>HoneyBee OBT</title>
         <HamburgerMenu />
