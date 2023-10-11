4c9454a88f85da9ff1bb52b2c45cc9d436d74035
Zac Poorman <poortexanlegos@gmail.com>
2023-10-10 19:31:28 -0600

    added formatting changes for json

--- src/pages/api/home.ts
+++ src/pages/api/home.ts
@@ -5,5 +5,28 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-    res.status(200).json({ record1: {name: "Story Name Here", description: "Example description", image: "/honeybee_logo.png", key: "story1", progress: true, completed: false}, record2: {name: "Story Name 2", description: "Here is example Description 2", image: "/honeybee_logo.png", key: "story2", progress: false, completed: true}, record3: {name: "Jesus Rising", description: "Here is more info", image: "/honeybee_logo.png", key: "story3", progress: true, completed: false}})
+    res.status(200).json({ 
+      record1: {
+        name: "Story Name Here", 
+        description: "Example description", 
+        image: "/honeybee_logo.png", 
+        key: "story1", 
+        progress: true, 
+        completed: false
+      }, record2: {
+        name: "Story Name 2", 
+        description: "Here is example Description 2", 
+        image: "/honeybee_logo.png", 
+        key: "story2", 
+        progress: false, 
+        completed: true
+      }, record3: {
+        name: "Jesus Rising", 
+        description: "Here is more info", 
+        image: "/honeybee_logo.png", 
+        key: "story3", 
+        progress: true, 
+        completed: false
+      }
+    })
 }
